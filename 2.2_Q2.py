de43a3d766a1484d767597cd96dfa40a4bdebf89
unknown <tamannajhanwar570gmail.com>
2025-06-11 22:49:36 +0530

    ch2

---
+++ 2.2_Q2.py
@@ -0,0 +1,8 @@
+#Q2
+
+a=1
+while a<=10:
+    if a==7:
+        break
+    print(a)
+    a=a+1
---
+++ 2.2_Q3.py
@@ -0,0 +1,7 @@
+#Q3
+
+v=['a','e','i','o','u']
+for i in 'python':
+    if i in v:
+        continue
+    print(i,end='')
